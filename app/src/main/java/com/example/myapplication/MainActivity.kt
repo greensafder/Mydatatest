f7961d50e4399ded6bab2a25c1f3d3e9bb6347d0
s <sayyedsafderali1324@gmail.com>
2021-12-03 17:04:15 +0530

    hhhh

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //hello
+        //hello ggggg
         setContentView(R.layout.activity_main)
     }
 }
